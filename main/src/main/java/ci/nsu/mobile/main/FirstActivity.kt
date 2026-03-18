9d1804387ba29f0b3837d0a7fb0ca058687116fa
HromovII <hromovii@mer.ci.nsu.ru>
2026-03-18 13:49:09 +0700

    TASK3 DONE

--- main/src/main/java/ci/nsu/mobile/main/FirstActivity.kt
+++ main/src/main/java/ci/nsu/mobile/main/FirstActivity.kt
@@ -19,16 +19,16 @@ class FirstActivity : AppCompatActivity() {
         buttonGoToSecond = findViewById(R.id.buttonGoToSecond)
 
         buttonGoToSecond.setOnClickListener {
-            // Получаем текст из EditText
+
             val userData = editTextData.text.toString()
 
-            // Создаём Intent для перехода на SecondActivity
+
             val intent = Intent(this, SecondActivity::class.java)
 
-            // Передаём данные через putExtra
+
             intent.putExtra("USER_DATA", userData)
 
-            // Запускаем SecondActivity
+
             startActivity(intent)
         }
     }
--- main/src/main/java/ci/nsu/mobile/main/SecondActivity.kt
+++ main/src/main/java/ci/nsu/mobile/main/SecondActivity.kt
@@ -1,8 +1,6 @@
 package ci.nsu.mobile.main
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -19,19 +17,19 @@ class SecondActivity : AppCompatActivity() {
         textViewReceivedData = findViewById(R.id.textViewReceivedData)
         toolbar = findViewById(R.id.toolbar)
 
-        // Настраиваем Toolbar как ActionBar
+
         setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true) // Показываем кнопку "Назад"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "Экран 2"
 
-        // Получаем данные из Intent
-        val receivedData = intent.getStringExtra("USER_DATA") ?: "Данные не получены"
+
+        val receivedData = intent.getStringExtra("USER_DATA")
         textViewReceivedData.text = "Полученные данные: $receivedData"
     }
 
-    // Обработка нажатия на кнопку "Назад" в Toolbar
+
     override fun onSupportNavigateUp(): Boolean {
-        finish() // Закрываем текущую Activity и возвращаемся назад
+        finish()
         return true
     }
 }
